bb4a4994c6bdfeda5475da440ca65b448a6c78c3
Jonathan Peña <97184743+JJPENA666@users.noreply.github.com>
2023-07-04 23:05:43 -0500

    first commit

---
+++ js/sweeterAlert.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: 'Gracias!',
+    text: 'Llena el formulario para contactarnos.',
+    imageUrl: 'https://www.latercera.com/resizer/GndLWohKS2aqkGn2Rxs1c2waids=/900x600/filters:focal(609x302:619x292)/cloudfront-us-east-1.images.arcpublishing.com/copesa/6BIH6PN6C5HY5FVJQ537FA3X3A.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
---
+++ js/swetA.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: 'Gracias!',
+    text: 'Esto es un poco de nosotros un gusto.',
+    imageUrl: 'https://www.lavanguardia.com/files/image_948_465/uploads/2018/06/29/5fa438bd08461.jpeg'   
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
